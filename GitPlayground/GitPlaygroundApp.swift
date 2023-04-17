50feafdbdb0af8b553b2c096024297f3d61d0171
Vladimir <motiw@icloud.com>
2023-04-17 16:09:26 +0600

    Initial Commit

---
+++ GitPlayground/GitPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPlaygroundApp.swift
+//  GitPlayground
+//
+//  Created by Vladimir on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
