d3b87d9aa6a1a10c2145a9fec57430be2bcf4d82
Vladimir <motiw@icloud.com>
2023-04-18 19:07:40 +0600

    Hello, three

--- GitPlayground/ContentView.swift
+++ GitPlayground/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, One!")
+            Text("Hello, Three!")
         }
         .padding()
     }
