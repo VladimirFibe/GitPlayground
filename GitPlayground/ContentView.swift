2e91a2f6292a554e79c20acbc9243bc60a040f78
Vladimir <motiw@icloud.com>
2023-04-24 15:41:46 +0600

    eight

--- GitPlayground/ContentView.swift
+++ GitPlayground/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Seven!")
+            Text("Hello, eight!")
         }
         .padding()
     }
